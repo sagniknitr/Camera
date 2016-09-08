dcf59bce5d407ca8188934a4a8af3395255df196
Danny <danny270793@gmail.com>
2016-09-08 00:29:01 -0500

    camera installer

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Camera',
+	version='1.8.9.0.25',
+	description='Raspberry camera libray for python',
+	author='Danny Vaca',
+	author_email='danny270793@gmail.com',
+	py_modules=['camera'],
+	)
